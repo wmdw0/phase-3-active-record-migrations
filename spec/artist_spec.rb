a57edeba4b106e79d67910becf91f21e18ae07b7
William Walsh <wmdwalsh0@gmail.com>
2021-10-30 16:21:36 -0400

    completed assignment

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -38,7 +38,5 @@ describe "Artist" do
     expect(Artist.column_names).not_to include("favorite_food")
   end
 
-  it "can migrate to have a favorite_flower attribute for Artist" do    
-    expect(Artist.column_names).to include("favorite_flower")
-  end
+  
 end
